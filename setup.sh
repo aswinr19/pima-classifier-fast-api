91f8f61b14e067fa822b3f6bb30311998a093944
aswinr <rashwin@zohotrainees.com>
2025-01-24 17:36:40 +0530

    updated dependencies and minor changes in setup file for tmux env

--- setup.sh
+++ setup.sh
@@ -6,9 +6,11 @@ tmux has-session -t $SESH 2>/dev/null
 
 if [ $? != 0 ]; then
   tmux new-session -d -s $SESH -n "neovim"
+  tmux send-keys -t $SESH:neovim "source bin/activate" C-m
   tmux send-keys -t $SESH:neovim "nvim src/main.py" C-m
 
   tmux new-window -t $SESH -n "run"
+  tmux send-keys -t $SESH:run "source bin/activate" C-m
   tmux send-keys -t $SESH:run "clear" C-m
 
   tmux select-window -t $SESH:neovim
